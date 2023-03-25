bb69b10ab0e106acf8d4f56f98e2ac1190a63527
Krutuja Desai <mendesismybusiness@gmail.com>
2023-03-25 08:15:29 +0530

    logo added

--- src/components/nav/Header.jsx
+++ src/components/nav/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ProgressBar from '../../utils/progressbar/ProgressBar'
-
+import logo from '../../assets/Dark background full logo.svg'
+import styles from './Header.module.css'
+// The header component displays the progress bar and logo
 const Header = () => {
   return (
+    <>
+    {/* Progress bar to show how many questions are completed */}
     <ProgressBar/>
+    {/* to display logo */}
+    <div className={styles.logo}>
+      <img src={logo} alt="logo"/>
+    </div>
+    </>
   )
 }
 
