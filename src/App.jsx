daa41533729f9d16dea0bf8b12b124ef5cdd2ff5
Krutuja Desai <mendesismybusiness@gmail.com>
2023-03-29 06:50:19 +0530

    logo animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,14 +8,22 @@ import SplashScreen from './components/splashscreen/SplashScreen'
 
 function App() {
   const [showSplash , setSplash] = useState(true)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSplash(false)
+    },3000)
+    return () => {
+      clearTimeout(timer)
+    }
+  })
   if(showSplash){
     return <SplashScreen/>
   }
    return (
-     <>
+     <div className='main'>
      <Header/>
      <Typeform/>
-     </>
+     </div>
    )
 }
 
